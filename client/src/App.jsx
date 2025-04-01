2b092bc939c8d328333f7532ea306f5770dd5e26
sanjanaynvsdl <sanjanayalamarthi@gmail.com>
2025-04-01 17:08:34 +0530

    intialized react-fe

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+   <div>
+    <p className='bg-yellow-300'>Web scrapper -amazon smart tv's</p>
+   </div>
+  )
+}
+
+export default App
